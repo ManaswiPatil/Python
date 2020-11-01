234c479a78d0566fb6cfa2aa303c35a3d710cab9
ManaswiPatil <50437663+ManaswiPatil@users.noreply.github.com>
2020-11-01 15:57:25 +1300

    Add files via upload

---
+++ Assignments/colorama_text.py
@@ -0,0 +1,6 @@
+from colorama import Fore, Back, Style
+print(Fore.RED + 'This text is in red colour')
+print(Back.YELLOW + 'Red text with Yellow Background')
+print(Style.DIM + 'Dimmed Red coloured text with Yellow Background')
+print(Style.RESET_ALL)
+print('Hello World!!')
